b412c94f3fcd0f177cc9b79890e06658337b2ddb
Nikolaj <git@undefine.dev>
2026-08-10 09:18:11 +0200

    [create-pull-request] automated change (#2)
    Co-authored-by: mostperfectboy <13489589+mostperfectboy@users.noreply.github.com>

--- pkgs/plannotator.nix
+++ pkgs/plannotator.nix
@@ -6,11 +6,11 @@
 
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "plannotator";
-  version = "0.25.1";
+  version = "0.26.7";
 
   src = fetchurl {
     url = "https://github.com/backnotprop/plannotator/releases/download/v${finalAttrs.version}/plannotator-linux-x64";
-    hash = "sha256-UWJWL7uX91SpyDwNdJ/GLSC6zeUJzV0v7MAEkL2d/m8=";
+    hash = "sha256-oZykQlyF0SU/rRS2iJv0Suw66b2ujq9kwdE+EiA2hBw=";
   };
 
   dontUnpack = true;
